f64613ef5be93e7aac329b0d2d78aa056a46bd34
ingridsl <inus2agome@gmail.com>
2017-10-20 09:30:27 -0200

    backup

--- LAB10.sql
+++ LAB10.sql
@@ -65,3 +65,44 @@ ELSE
 GO
 -- challenge 2
 -- 1
+
+SELECT * FROM SALESLT.Product;
+SELECT * FROM SALESLT.ProductCategory;
+
+DECLARE @MarketAverage money = 2000;
+DECLARE @MarketMax int = 5000;
+DECLARE @AWMax money;
+DECLARE @AWAverage money;
+
+SELECT @AWAverage =  AVG(ListPrice), @AWMax = MAX(ListPrice)
+FROM SalesLT.Product
+WHERE ProductCategoryID IN
+	(SELECT DISTINCT ProductCategoryID
+	 FROM SalesLT.vGetAllCategories
+	 WHERE ParentProductCategoryName = 'Bikes');
+
+
+WHILE @AWAverage < @MarketAverage 
+	BEGIN
+		UPDATE SALESLT.PRODUCT
+		SET LISTPRICE = LISTPRICE * 1.1
+		WHERE ProductCategoryID IN (SELECT DISTINCT ProductCategoryID
+									FROM SalesLT.vGetAllCategories
+									WHERE ParentProductCategoryName = 'Bikes');
+
+		SELECT @AWAverage =  AVG(ListPrice), @AWMax = MAX(ListPrice)
+		FROM SalesLT.Product
+		WHERE ProductCategoryID IN (SELECT DISTINCT ProductCategoryID
+									FROM SalesLT.vGetAllCategories
+									WHERE ParentProductCategoryName = 'Bikes');
+
+		IF @AWMax >= @MarketMax
+			BREAK
+		ELSE
+			CONTINUE
+
+
+
+	END
+PRINT 'New average bike price:' + CONVERT(varchar, @AWAverage);
+PRINT 'New maximum bike price:' + CONVERT(varchar, @AWMax);
